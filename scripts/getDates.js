e998a2e347bfcb316d466eb19b87462dcf9a0e17
Ty Saltzgiver <118573450+Saltzmaster@users.noreply.github.com>
2024-02-03 10:47:40 -0700

    dark mode + menu icon

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -10,3 +10,21 @@ const lastMotifiedElement = document.querySelector('#lastModified');
 const lastMotified = document.lastModified;
 
 lastMotifiedElement.innerHTML = `Last Motified: ${lastMotified}`;
+
+document.addEventListener('DOMContentLoaded', function () {
+    var menuIcon = document.querySelector('.menu-icon');
+    var navLinks = document.querySelector('nav');
+
+    menuIcon.addEventListener('click', function () {
+        navLinks.classList.toggle('show-menu');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var darkModeToggle = document.querySelector('.dark-mode');
+    var body = document.body;
+
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark');
+    });
+});

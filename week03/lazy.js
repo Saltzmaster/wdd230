b895fb79c42f7b0f13ac59f71a2640b89ec0e353
Ty Saltzgiver <118573450+Saltzmaster@users.noreply.github.com>
2024-02-10 11:11:24 -0700

    Lazy Load

---
+++ week03/lazy.js
@@ -0,0 +1,4 @@
+const lastMotifiedElement = document.querySelector('#lastModified');
+const lastMotified = document.lastModified;
+
+lastMotifiedElement.innerHTML = `Last Motified: ${lastMotified}`;
